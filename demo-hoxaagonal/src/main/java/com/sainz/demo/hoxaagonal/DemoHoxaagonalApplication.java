c5f8a38b9c6c0454843f8c9c2e141cbc1b0b0be6
D-Sainz <diego.sainz.e@gmail.com>
2023-09-15 17:59:26 -0400

    Primer commit creacion del proyecto con sprint initializr

---
+++ demo-hoxaagonal/src/main/java/com/sainz/demo/hoxaagonal/DemoHoxaagonalApplication.java
@@ -0,0 +1,13 @@
+package com.sainz.demo.hoxaagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHoxaagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHoxaagonalApplication.class, args);
+	}
+
+}
---
+++ demo-hoxaagonal/src/test/java/com/sainz/demo/hoxaagonal/DemoHoxaagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sainz.demo.hoxaagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHoxaagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
